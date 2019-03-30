8d2c750d1c1c73e5cb5858b5eedeb1d6a3ae2712
danvop <danvop@gmail.com>
2019-03-30 18:23:27 +1000

    make site

---
+++ site-contact-form/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+    <!-- <script src="main.js"></script> -->
+</head>
+<body>
+    <div class="featured">
+        <p>featured sentence</p>
+    </div>
+    <div class="contact"></div>
+    <div class="expert"></div>
+</body>
+</html>
