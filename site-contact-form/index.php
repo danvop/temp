934979d1e5ff87633724fff4451450233cc5fdef
danvop <danvop@gmail.com>
2019-03-31 17:50:27 +1000

    change prices

--- site-contact-form/index.php
+++ site-contact-form/index.php
@@ -10,9 +10,88 @@
 </head>
 <body>
     <div class="featured">
-                
+        <div class="featured-words-header">
+            <p>Featured words</p>
+        </div>
+
+        <div class="featured-words-button">
+            <a href="#expert">get</a>    
+        </div>
     </div>
-    <div class="contact"></div>
-    <div class="expert"></div>
+
+    <div class="expert" id="expert"></div>
+        <div class="expert-media">
+            <div class="expert-media-img">
+
+            </div>
+            <div class="expert-media-header">
+
+            </div>
+            <div class="expert-media-body">
+
+            </div>
+        </div>
+    <div class="contact">
+        <div class="contact-form">
+        <form action="">
+            <input type="email" required>
+            <input type="submit" class="submit-btn" value="запросить">
+        </form>
+        </div>
+        <div class="contact-prices">
+            <div class="contact-prices-box">
+                <div class="contact-prices-box-header">
+                    <p>Вводный курс</p>
+                </div>
+                <div class="contact-prices-box-content">
+                    <ul>
+                        <li>Простая консультация</li>
+                        <li>Поверхностный обзор</li>
+                        <li>Нельзя трогать оборудование</li>
+                        <li>Можно 10 минут посмотреть на коммутатор в присутствии эксперта</li>
+                    </ul>
+                </div>
+
+                <div class="contact-prices-box-price">
+                    <p>5000 &#8381;</p>
+                </div>
+                
+            </div>
+            <div class="contact-prices-box">
+                <div class="contact-prices-box-header">
+                    <p>Углубленный курс</p>
+                </div>
+                <div class="contact-prices-box-content">
+                    <ul>
+                        <li>Простая консультация</li>
+                        <li>Поверхностный обзор</li>
+                        <li>Разрешат потрогать патчкорд</li>
+                        <li>Безлимитный доступ на просмотр моргающих линков</li>
+                    </ul>
+                </div>
+
+                <div class="contact-prices-box-price">
+                    <p>12000 &#8381;</p>
+                </div>
+            </div>
+            <div class="contact-prices-box">
+                <div class="contact-prices-box-header"> 
+                    <p>Полное погружение</p>
+                </div>
+                <div class="contact-prices-box-content">
+                    <ul>
+                        <li>Расширенная беседа</li>
+                        <li>Обсуждение важных функций</li>
+                        <li>Разрешат зайти в консоль</li>
+                    </ul>
+                </div>
+
+                <div class="contact-prices-box-price">
+                    <p>30000 &#8381;</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    
 </body>
 </html>
