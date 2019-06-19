d8eadf814966b1aefaf245328df2e2b0459fc8b8
danvop <danvop@gmail.com>
2019-06-19 17:52:19 +1000

    act

--- input-validation/act.php
+++ input-validation/act.php
@@ -6,6 +6,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // the request method is fine
         if (!empty($_POST['email']) || !empty($_POST['phone'])) {
                 exit("OK");
+        } else {
+                
         }
 
         if(!empty($_POST['email'])) {
