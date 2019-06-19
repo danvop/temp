e25d4e1e93bdeaba2bf10492b32717a56c387348
danvop <danvop@gmail.com>
2019-06-19 22:46:58 +1000

    fix styles

--- input-validation/index.php
+++ input-validation/index.php
@@ -1,8 +1,28 @@
-<form method="POST" action="act.php">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        input {
+            display: block;
+            margin: 10px;
+            padding: 10px;
+            border-radius: 5px;
+        }
+    </style>
+</head>
+<body>
+    <form method="POST" action="act.php">
 
-    <!-- some form elements here -->
-    <input type="email">
-    <input type="tel" id="phone" name="phone" pattern="[0-9]{10}">
-    <input type="submit">
+        <!-- some form elements here -->
+        <input type="email" style="display:block">
+        <input type="tel" id="phone" name="phone" pattern="[0-9]{10}">
+        <input type="checkbox" name="subscribe" id="subscribe">
+        <input type="submit">
 
-</form>
+    </form>
+</body>
+</html>
